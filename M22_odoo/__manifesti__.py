a8e4244c7918871b916690bbd06ec02581af4a61
Daniel Palacios <daniel@m22.mx>
2022-01-17 22:47:17 +0000

    [ADD] Modulo Basico

---
+++ M22_odoo/__manifesti__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'M22 Prubea', 
+    'summary': """Aplicación de Prueba""",
+    'desription': "Esta es una prubea para gestionar información",
+    'author': 'VDPC',
+    'website': 'https://www.m22.mx',
+    'category': 'Capacitación',
+    'version': '0.1',
+    
+    'depends': ['base'],
+    'data': [],
+    'demo'_ [],
+    
+    
+}
