f28213abc641844429389b06b8e88cd981328505
Daniel Palacios <daniel@m22.mx>
2022-01-19 02:54:16 +0000

    Día 2: Modelos

---
+++ M22_odoo/models/course.py
@@ -0,0 +1,9 @@
+# -*- codig: utf-8 -*-
+
+from odoo  import models, fields, api
+
+class Course(models.Model):
+    
+    _name = 'academia.course'
+    _description = 'Courso de capacitación'
+    
